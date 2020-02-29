c2c9505df205916c4daffd4eac67b86f49a12d92
jezinka <jezina1989@gmail.com>
2020-02-29 15:58:48 +0100

    processing visualization

--- src/main/groovy/SudokuBacktracking.groovy
+++ src/main/groovy/SudokuBacktracking.groovy
@@ -1,43 +1,80 @@
-class SudokuBacktracking {
+import processing.core.PApplet
+import processing.core.PFont
+
+class SudokuBacktracking extends PApplet {
 
     List<List<Integer>> board
+    Map<String, Boolean> isFixedNumber = [:]
     int rowIndex, columnIndex
+
+    PFont f
+    String textOutput = ''
 
     SudokuBacktracking(List<List<Integer>> board) {
         this.board = board
         this.rowIndex = 0
         this.columnIndex = 0
+        this.board.eachWithIndex { List<Integer> row, int i -> row.eachWithIndex { int col, int j -> this.isFixedNumber.put("$i$j".toString(), col != 0) } }
     }
 
     void resolve() {
-        Map<String, Boolean> isFixedNumber = [:]
-        board.eachWithIndex { List<Integer> row, int i -> row.eachWithIndex { int col, int j -> isFixedNumber.put("$i$j".toString(), col != 0) } }
-        printTable()
+        String[] processingArgs = { "Sudoku backtracking" }
+        runSketch(processingArgs, this)
+    }
 
-        while (board.flatten().any { it == 0 }) {
-            if (isFixedNumber["$rowIndex$columnIndex" as String]) {
-                nextCell()
-            } else {
-                int cell = board[rowIndex][columnIndex]
-                int value = ++cell
-                if (value <= 9) {
-                    board[rowIndex][columnIndex] = value
-                    if (this.boardValidation()) {
-                        nextCell()
-                    }
-                } else {
-                    board[rowIndex][columnIndex] = 0
-                    do {
-                        previousCell()
-                    } while (isFixedNumber["$rowIndex$columnIndex" as String])
+    void settings() {
+        size(200, 250)
+    }
+
+    void setup() {
+        frameRate = 30
+        f = createFont("Arial", 16, true)
+    }
+
+    void draw() {
+        background(255)
+        textFont(f, 16)
+        fill(0)
+        drawBoard()
+        text(this.textOutput, 20, 20)
+        if (board.flatten().any { it == 0 }) {
+            step()
+        } else {
+            text("Done", 190, 210)
+            noLoop()
+        }
+    }
+
+    private void drawBoard() {
+        line(64, 0, 64, 220)
+        line(110, 0, 110, 220)
+        line(20, 72, 160, 72)
+        line(20, 145, 160, 145)
+    }
+
+    private step() {
+        if (isFixedNumber["$rowIndex$columnIndex" as String]) {
+            nextCell()
+        } else {
+            int cell = board[rowIndex][columnIndex]
+            int value = ++cell
+            if (value <= 9) {
+                board[rowIndex][columnIndex] = value
+                if (this.boardValidation()) {
+                    nextCell()
                 }
+            } else {
+                board[rowIndex][columnIndex] = 0
+                do {
+                    previousCell()
+                } while (isFixedNumber["$rowIndex$columnIndex" as String])
             }
         }
         printTable()
     }
 
     private printTable() {
-        println board.collect { it.join(',') }.join('\n') + '\n'
+        this.textOutput = board.collect { it.join(' ') }.join('\n') + '\n'
     }
 
     private void nextCell() {
